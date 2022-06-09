dfadbd416885012d60325996c2243223cad56c1a
Roque Vera <roquegv@gmail.com>
2022-06-09 15:12:17 +0000

    crear doctype

---
+++ armando_app/armando_app/doctype/formulario_principal/formulario_principal.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Roque Vera and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Formulario Principal', {
+	// refresh: function(frm) {
+
+	// }
+});
