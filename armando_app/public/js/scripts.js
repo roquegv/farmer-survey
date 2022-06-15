587a487a443c63972ab67527d69bbd47997604ac
Roque Vera <roquegv@gmail.com>
2022-06-15 17:55:57 +0000

    if dentro de la funcion

--- armando_app/public/js/scripts.js
+++ armando_app/public/js/scripts.js
@@ -1,18 +1,16 @@
-var route_to_new_doc = function(frm){
-    local_docname = frappe.model.make_new_doc_and_get_name('Formulario Principal')
-    frappe.set_route('Form', 'Formulario Principal', local_docname)
+var route_to_new_doc = function(){
+    if (frappe.user.has_role("Operador")){
+        local_docname = frappe.model.make_new_doc_and_get_name('Formulario Principal')
+        frappe.set_route('Form', 'Formulario Principal', local_docname)
+    }
 }
 
 $(document).on("startup", function () {
-    if (frappe.user.has_role("Operador")){
-        route_to_new_doc()
-    }
+    route_to_new_doc()
 });
 
 frappe.ui.form.on("Formulario Principal", {
     on_submit: function(frm){
-        if (frappe.user.has_role("Operador")){
-            route_to_new_doc()
-        }
+        route_to_new_doc()
     }
 });
