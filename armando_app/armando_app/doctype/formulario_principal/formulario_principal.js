119332b831223bbf00b517122dd508b180433b53
Roque Vera <roquegv@gmail.com>
2022-06-21 18:15:31 +0000

    on_submit

--- armando_app/armando_app/doctype/formulario_principal/formulario_principal.js
+++ armando_app/armando_app/doctype/formulario_principal/formulario_principal.js
@@ -2,8 +2,7 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Formulario Principal', {
-	refresh: function(frm) {
-		console.log("refresh");
+	refresh(frm) {
 		var map = frm.get_field("ubicacion").map
 		map.setView([-24.9014724,-56.2298799], 7);
 		
@@ -12,9 +11,34 @@ frappe.ui.form.on('Formulario Principal', {
 		$(".leaflet-draw-draw-rectangle").remove()
 		$(".leaflet-draw-draw-circle").remove()
 		$(".leaflet-draw-draw-circlemarker").remove()
+		$(".leaflet-draw-draw-marker").remove()
+		$(".leaflet-draw-edit-edit").remove()
+		$(".leaflet-draw-edit-remove").remove()
+		
+		if (frm.is_new()) {
+		    frm.set_value("usuario_del_operador", frappe.user.name)
+		    frm.set_value("nombre_del_operador", frappe.user.full_name())
+		    
+		    navigator.geolocation.getCurrentPosition(function(location) {
+    			var latlng = new L.LatLng(location.coords.latitude, location.coords.longitude);
+    			var marker = L.marker(latlng, {id:"ubicacion_marker", draggable:'true'})
+    			frm.set_value("ubicacion", JSON.stringify(marker.toGeoJSON()))
+    		})
+		}
+		
+		$(".btn-attach").click(() => {
+            setTimeout(()=>{
+                $(".btn-modal-secondary").hide()
+                if ($(".btn-file-upload").length == 4){
+                    $(".btn-file-upload").first().remove()
+                    $(".btn-file-upload").first().remove()
+                    $(".btn-file-upload").first().remove()
+                }
+            }, 300)
+        })
 	},
 
-	after_save: function(frm){
+	on_submit: function(frm){
 		frappe.db.count('Formulario Principal', {
 			filters: {'usuario_del_operador': frappe.user.name}
 		}).then(r=>{
